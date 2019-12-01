69f440193bf93834dd7603ce3adea711c98c938f
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2019-12-01 14:26:31 -0300

    Empezando curso php

---
+++ blank.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> PHP course</title>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+</head>
+<body>	
+		<? $name = "sitio de josesin" ?>
+
+		<h1> <? echo $name; ?> </h1>	
+
+
+
+
+	<script src="bootstrap/js/bootstrap.js"></script>
+	<script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+
+</body>
+</html>
+	
